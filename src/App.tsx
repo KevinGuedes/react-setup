7bc4f7717eb6e8e42de322dd293ea22865da45ae
KevinGuedes <kevinguedes@edu.unifor.br>
2022-08-16 19:48:28 -0300

    feat(commitlint): commitlint setup

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App(): JSX.Element {
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react2" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
